4030047eca0938bb5a906386c3159a0eee20e577
knrdl <knrdl@knrdl>
2022-02-17 14:22:28 +0100

    chore(client): disable source maps

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -33,7 +33,7 @@ function serve() {
 export default {
 	input: 'src/main.ts',
 	output: {
-		sourcemap: true,
+		sourcemap: false,
 		format: 'iife',
 		name: 'app',
 		file: 'public/build/bundle.js'
