3fdc7929e1a467e173f4ae463241a561cca8ac97
knrdl <35548889+knrdl@users.noreply.github.com>
2023-04-05 16:16:00 +0200

    fix: broken demo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '', // is "/" per default (static path) so serving from a subfolder does not work => empty base equals "./" (relative paths)
 })
